9923e0fe6c6773c6c9a02e652a4a5a77650de9c3
Brian Gillespie <brian.j.gillespie@gmail.com>
2018-11-15 00:15:09 +0000

    cleanup

--- lab-1-solns/longest.sh
+++
@@ -1,9 +0,0 @@
-awk '
-  BEGIN { longest = 0 }
-        {
-          if (length($0) > longest) {
-            longest = length($0)
-          }
-        } 
-  END   { print longest }
-' $1
--- lab-1-solns/shortest.sh
+++
@@ -1,9 +0,0 @@
-awk '
-  BEGIN { shortest = 9999 }
-        {
-          if (length($0) < shortest) {
-            shortest = length($0)
-          }
-        } 
-  END   { print shortest }
-' $1
