b6bd9caaca3aa9ea5cc23df5f1ad4f1e926b7157
Cat <c16371033@mydit.ie>
2018-11-22 11:00:00 +0000

    made it a proxy

--- lab-5-starter/my_application/server.py
+++ lab-5-starter/my_application/server.py
@@ -1,6 +1,12 @@
 from flask import Flask
+import requests
 app = Flask(__name__)
 
-@app.route("/")
-def hello():
-    return "Hello World!"
+@app.route('/', defaults={'path': ''})
+@app.route('/<path:path>')
+def catch_all(path):
+    r = requests.get('http://jsonplaceholder.typicode.com/%s' % path)
+    return r.text
+
+if __name__ == '__main__':
+    app.run()
